6d000fd1e740f548b2880f33d941101919fba85c
Martin Buist <biebml@nus.edu.sg>
2018-07-06 09:57:22 +0800

    Fixing the header guards

--- UnitTestCustomUtilities.hpp
+++ UnitTestCustomUtilities.hpp
@@ -17,8 +17,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
-#ifndef CBL_UNITTESTCUSTOMUTILITIES_HPP_
-#define CBL_UNITTESTCUSTOMUTILITIES_HPP_
+#ifndef UNITTESTCUSTOMUTILITIES_HPP_
+#define UNITTESTCUSTOMUTILITIES_HPP_
 
 #include <algorithm>
 #include <string>
